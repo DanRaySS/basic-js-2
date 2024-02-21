b1c3a62de89085d22bd575c6aed1e23ccb2965aa
Daniil <danilmux@gmail.com>
2024-02-21 22:35:12 +0500

    rest

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 'use strict';
 
+const log = function (a, b, ...rest) {
+    console.log(a, b, rest);
+};
+
+// log('basic', 'rest', 'operator', 'usage');
+log('basic', 'rest', 'operator');
+
+function calcOrDouble(number, basis = 2) {
+    // basis = basis || 2;
+    console.log(number * basis);
+}
+
+calcOrDouble(3);
